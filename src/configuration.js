c6c897cad21ced6c93ed23218d371c022b82b106
Paolo Ferretti <paolo@paoloferretti.net>
2017-07-19 11:36:18 +0200

    Refs #61 - New check and merge functions for configuration loading

--- src/configuration.js
+++ src/configuration.js
@@ -1,18 +1,44 @@
 const fs = require('fs');
+const path = require('path');
 
-function loader(configPath) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(configPath, function(err, data) {
-      if (err) {
-        return reject(err);
-      }
+function check(configPath, defaultPath) {
+  if (configPath !== defaultPath) {
+    return true;
+  }
+  
+  const exist = fs.existsSync(defaultPath);
+  if (configPath === defaultPath && exist) {
+    return true;
+  }
+  return false;
+}
 
-      const parsedData = JSON.parse(data);
-      resolve(parsedData);
-    });
-  });
+function load(configPath) {
+  const fileContent = fs.readFileSync(configPath, 'utf-8');
+  return JSON.parse(fileContent);
+}
+
+function merge(configObj, program) {
+  const commandLineConf = {
+    port: program.port,
+    path: program.path ? path.resolve(program.path) : null,
+    endpoint: program.endpoint,
+    'pfer-api': program.preferApi,
+    log: program.log,
+    root: program.root,
+    dynpath: program.dynpath,
+    timeout: program.timeout
+  };
+
+  Object
+    .keys(commandLineConf)
+    .forEach(key => (commandLineConf[key] == null) && delete commandLineConf[key]);
+
+  return Object.assign({}, configObj, commandLineConf);
 }
 
 module.exports = {
-  loader: loader
+  check: check,
+  load: load,
+  merge: merge
 };
